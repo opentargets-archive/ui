531625ba7d523ea09ad8f98f5508c3ab45f44da9
peatroot <garethpeat@gmail.com>
2019-10-09 13:40:09 +0100

    Add and use theme

--- packages/genetics-app/src/App.tsx
+++ packages/genetics-app/src/App.tsx
@@ -1,37 +1,42 @@
 import React from "react";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import { Page, Header, Footer } from "ui";
+import { Page, Header, Footer, theme } from "ui";
 
 import HomePage from "./home/Page";
 import GenePage from "./gene/Page";
 import DiseasePage from "./disease/Page";
 
 const App: React.FC = () => (
-  <Router>
-    <Switch>
-      {/* home is special, as no header/footer is shown */}
-      <Route exact path="/">
-        <Page>
-          <HomePage />
-        </Page>
-      </Route>
+  // theme can be overridden; see material-ui docs for detail
+  <ThemeProvider theme={createMuiTheme(theme)}>
+    <Router>
+      <Switch>
+        {/* home is special, as no header/footer is shown */}
+        <Route exact path="/">
+          <Page>
+            <HomePage />
+          </Page>
+        </Route>
 
-      {/* all other pages show header/footer */}
-      <Page
-        header={<Header platformName="Genetics" />}
-        footer={<Footer version="0.0.0" />}
-      >
-        <Route path="/gene/:ensgId">
-          <GenePage />
-        </Route>
-        <Route path="/disease/:efoId">
-          <DiseasePage />
-        </Route>
-      </Page>
-    </Switch>
-  </Router>
+        {/* all other pages show header/footer */}
+        <Page
+          header={<Header platformName="Genetics" />}
+          footer={<Footer version="0.0.0" />}
+        >
+          <Route path="/gene/:ensgId">
+            <GenePage />
+          </Route>
+          <Route path="/disease/:efoId">
+            <DiseasePage />
+          </Route>
+        </Page>
+      </Switch>
+    </Router>
+  </ThemeProvider>
 );
 
 export default App;
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -4,3 +4,5 @@ export { default as ExampleComponent } from "./components/ExampleComponent";
 export { default as Page } from "./components/Page";
 export { default as Header } from "./components/Header";
 export { default as Footer } from "./components/Footer";
+
+export { default as theme } from "./theme";
