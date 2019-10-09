baa250f688c72a6f835e405610ea1f44e82b531a
peatroot <garethpeat@gmail.com>
2019-10-09 14:56:31 +0100

    Add ApolloProvider and .env.development files

--- packages/genetics-app/src/App.tsx
+++ packages/genetics-app/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
 import { ThemeProvider } from "@material-ui/styles";
 import { createMuiTheme } from "@material-ui/core/styles";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 import { Page, Header, Footer, FontAwesomeLoader, theme } from "ui";
@@ -10,35 +15,44 @@ import HomePage from "./home/Page";
 import GenePage from "./gene/Page";
 import DiseasePage from "./disease/Page";
 
-const App: React.FC = () => (
-  // theme can be overridden; see material-ui docs for detail
-  <ThemeProvider theme={createMuiTheme(theme)}>
-    <FontAwesomeLoader>
-      <Router>
-        <Switch>
-          {/* home is special, as no header/footer is shown */}
-          <Route exact path="/">
-            <Page>
-              <HomePage />
-            </Page>
-          </Route>
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: process.env.REACT_APP_GENETICS_API_URL,
+  }),
+  cache: new InMemoryCache(),
+});
 
-          {/* all other pages show header/footer */}
-          <Page
-            header={<Header platformName="Genetics" />}
-            footer={<Footer version="0.0.0" />}
-          >
-            <Route path="/gene/:ensgId">
-              <GenePage />
+const App: React.FC = () => (
+  <ApolloProvider client={client}>
+    {/* theme can be overridden; see material-ui docs for detail */}
+    <ThemeProvider theme={createMuiTheme(theme)}>
+      <FontAwesomeLoader>
+        <Router>
+          <Switch>
+            {/* home is special, as no header/footer is shown */}
+            <Route exact path="/">
+              <Page>
+                <HomePage />
+              </Page>
             </Route>
-            <Route path="/disease/:efoId">
-              <DiseasePage />
-            </Route>
-          </Page>
-        </Switch>
-      </Router>
-    </FontAwesomeLoader>
-  </ThemeProvider>
+
+            {/* all other pages show header/footer */}
+            <Page
+              header={<Header platformName="Genetics" />}
+              footer={<Footer version="0.0.0" />}
+            >
+              <Route path="/gene/:ensgId">
+                <GenePage />
+              </Route>
+              <Route path="/disease/:efoId">
+                <DiseasePage />
+              </Route>
+            </Page>
+          </Switch>
+        </Router>
+      </FontAwesomeLoader>
+    </ThemeProvider>
+  </ApolloProvider>
 );
 
 export default App;
