de3d0d987cbd05bc15b6bfd95f6d0e0aa63c1101
peatroot <garethpeat@gmail.com>
2019-10-10 14:11:59 +0100

    Add example Sections with styling

--- packages/platform-app/src/gene/Page.tsx
+++ packages/platform-app/src/gene/Page.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { Avatar } from "@material-ui/core";
+import { Section, SectionContainer } from "ui";
 
-const GenePage: React.FC = () => <div>Gene!</div>;
+const GenePage: React.FC = () => (
+  <SectionContainer>
+    <Section
+      title="Section 1"
+      subtitle="Some subtitle text"
+      avatar={<Avatar>S1</Avatar>}
+    >
+      Content
+    </Section>
+    <Section
+      title="Section 2"
+      subtitle="Some subtitle text"
+      avatar={<Avatar>S2</Avatar>}
+    >
+      Content
+    </Section>
+    <Section
+      title="Section 3"
+      subtitle="Some subtitle text"
+      avatar={<Avatar>S3</Avatar>}
+    >
+      Content
+    </Section>
+  </SectionContainer>
+);
 
 export default GenePage;
--- packages/ui/src/theme.ts
+++ packages/ui/src/theme.ts
@@ -1,10 +1,12 @@
+import { grey } from "@material-ui/core/colors";
+
 const PRIMARY = "#3489ca";
 const SECONDARY = "#ff6350";
 
 const theme = {
-  // shape: {
-  //   borderRadius: 0,
-  // },
+  shape: {
+    borderRadius: 0,
+  },
   typography: {
     useNextVariants: true,
     fontFamily: '"Inter", "serif"',
@@ -28,95 +30,103 @@ const theme = {
   },
   // sectionPadding: '8px',
   // widgetHeight: '250px',
-  // props: {
-  //   MuiTab: {
-  //     disableRipple: true,
-  //   },
-  // },
-  // overrides: {
-  //   MuiButton: {
-  //     root: {
-  //       borderRadius: 0,
-  //       border: 0,
-  //       padding: '6px 12px',
-  //       minWidth: '32px',
-  //       minHeight: '32px',
-  //       height: '32px',
-  //       textTransform: 'none',
-  //     },
-  //   },
-  //   MuiCard: {
-  //     root: {
-  //       border: `1px solid #ddd`,
-  //     },
-  //   },
-  //   MuiIconButton: {
-  //     root: {
-  //       width: '32px',
-  //       height: '32px',
-  //       padding: '0px',
-  //     },
-  //   },
-  //   MuiTablePagination: {
-  //     root: {
-  //       height: '36px',
-  //       minHeight: '36px',
-  //     },
-  //     toolbar: {
-  //       height: '36px',
-  //       minHeight: '36px',
-  //     },
-  //   },
-  //   MuiTabs: {
-  //     root: {
-  //       borderBottom: '1px solid #616161',
-  //     },
-  //     indicator: {
-  //       display: 'none',
-  //     },
-  //   },
-  //   MuiTab: {
-  //     root: {
-  //       textTransform: 'none',
-  //       minWidth: '10px !important',
-  //       '&$selected': {
-  //         backgroundColor: PRIMARY,
-  //         color: 'white',
-  //         '&:hover': { backgroundColor: PRIMARY },
-  //       },
-  //       '&:hover': { backgroundColor: lighten(0.3, PRIMARY) },
-  //     },
-  //   },
-  //   MuiExpansionPanelSummary: {
-  //     root: {
-  //       padding: 0,
-  //       paddingRight: '32px',
-  //       minHeight: 0,
-  //       '&$expanded': {
-  //         minHeight: 0,
-  //         margin: 0,
-  //       },
-  //     },
-  //     content: {
-  //       width: '100%',
-  //       margin: 0,
-  //       '&$expanded': {
-  //         margin: 0,
-  //       },
-  //     },
-  //   },
-  //   MuiExpansionPanelDetails: {
-  //     root: {
-  //       padding: 0,
-  //       paddingRight: '32px',
-  //     },
-  //   },
-  //   MuiLinearProgress: {
-  //     root: {
-  //       height: '1px',
-  //     },
-  //   },
-  // },
+  props: {
+    //   MuiTab: {
+    //     disableRipple: true,
+    //   },
+    MuiCard: {
+      elevation: 0,
+    },
+  },
+  overrides: {
+    //   MuiButton: {
+    //     root: {
+    //       borderRadius: 0,
+    //       border: 0,
+    //       padding: '6px 12px',
+    //       minWidth: '32px',
+    //       minHeight: '32px',
+    //       height: '32px',
+    //       textTransform: 'none',
+    //     },
+    //   },
+    MuiCard: {
+      root: {
+        border: `1px solid ${grey[300]}`,
+      },
+    },
+    MuiCardHeader: {
+      root: {
+        borderBottom: `1px solid ${grey[300]}`,
+      },
+    },
+    //   MuiIconButton: {
+    //     root: {
+    //       width: '32px',
+    //       height: '32px',
+    //       padding: '0px',
+    //     },
+    //   },
+    //   MuiTablePagination: {
+    //     root: {
+    //       height: '36px',
+    //       minHeight: '36px',
+    //     },
+    //     toolbar: {
+    //       height: '36px',
+    //       minHeight: '36px',
+    //     },
+    //   },
+    //   MuiTabs: {
+    //     root: {
+    //       borderBottom: '1px solid #616161',
+    //     },
+    //     indicator: {
+    //       display: 'none',
+    //     },
+    //   },
+    //   MuiTab: {
+    //     root: {
+    //       textTransform: 'none',
+    //       minWidth: '10px !important',
+    //       '&$selected': {
+    //         backgroundColor: PRIMARY,
+    //         color: 'white',
+    //         '&:hover': { backgroundColor: PRIMARY },
+    //       },
+    //       '&:hover': { backgroundColor: lighten(0.3, PRIMARY) },
+    //     },
+    //   },
+    //   MuiExpansionPanelSummary: {
+    //     root: {
+    //       padding: 0,
+    //       paddingRight: '32px',
+    //       minHeight: 0,
+    //       '&$expanded': {
+    //         minHeight: 0,
+    //         margin: 0,
+    //       },
+    //     },
+    //     content: {
+    //       width: '100%',
+    //       margin: 0,
+    //       '&$expanded': {
+    //         margin: 0,
+    //       },
+    //     },
+    //   },
+    //   MuiExpansionPanelDetails: {
+    //     root: {
+    //       padding: 0,
+    //       paddingRight: '32px',
+    //     },
+    //   },
+    //   MuiLinearProgress: {
+    //     root: {
+    //       height: '1px',
+    //     },
+    //   },
+  },
 };
 
 export default theme;
