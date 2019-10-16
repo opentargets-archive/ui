b72855e64492fd84019d2e5a38aa623b0c79a5ce
peatroot <garethpeat@gmail.com>
2019-10-16 14:38:32 +0100

    Add filter placeholder

--- packages/ui/src/components/TableClientSideFilter.tsx
+++ packages/ui/src/components/TableClientSideFilter.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Select, MenuItem } from "@material-ui/core";
 
 export type TableClientSideFilterProps = {
+  placeholder?: string;
   onFilterChange: (value: string[]) => void;
   filterOptions: {
     label: React.ReactElement | string;
@@ -11,15 +12,24 @@ export type TableClientSideFilterProps = {
 };
 
 const TableClientSideFilter: React.FC<TableClientSideFilterProps> = ({
+  placeholder = "...",
   filterOptions,
   onFilterChange,
 }) => (
   <Select
     multiple
+    displayEmpty
     value={filterOptions.filter(o => o.selected).map(o => o.value)}
     onChange={(event: React.ChangeEvent<{ value: unknown }>) => {
       const selected = event.target.value as string[];
       onFilterChange(selected);
+    }}
+    renderValue={selected => {
+      if ((selected as string[]).length === 0) {
+        return <em>{placeholder}</em>;
+      }
+
+      return (selected as string[]).join(", ");
     }}
   >
     {filterOptions.map(o => (
