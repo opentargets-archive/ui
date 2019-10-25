5e4a416d770c454d9e239e15679febf13131e15e
peatroot <garethpeat@gmail.com>
2019-10-25 09:47:10 +0100

    Fix negative margin issue in Footer

--- packages/ui/src/components/Footer.tsx
+++ packages/ui/src/components/Footer.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Box, Grid, Typography } from "@material-ui/core";
 
 type Props = {
   version: string;
 };
 
 const Footer: React.FC<Props> = ({ version }) => (
-  <Grid container spacing={3} justify="center">
-    <Grid item xs={12}>
-      <Typography>Version {version}</Typography>
+  <Box m={0} p={3}>
+    <Grid container spacing={3} justify="center">
+      <Grid item xs={12}>
+        <Typography>Version {version}</Typography>
+      </Grid>
     </Grid>
-  </Grid>
+  </Box>
 );
 
 export default Footer;
