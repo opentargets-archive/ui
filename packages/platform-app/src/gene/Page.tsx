e513f7d6df544e5dda5a6dfe39994fc1acec0013
peatroot <garethpeat@gmail.com>
2019-11-05 15:53:02 +0000

    Add tabs to platform gene page

--- packages/platform-app/src/gene/Page.tsx
+++ packages/platform-app/src/gene/Page.tsx
@@ -1,39 +1,79 @@
 import React from "react";
 import { Avatar, Chip } from "@material-ui/core";
-import { Section, SectionContainer, ChipContainer } from "ui";
+import {
+  Tabs,
+  Section,
+  SectionContainer,
+  ChipContainer,
+  PageTitle,
+  GeneIcon,
+} from "ui";
 
+const tabs = [
+  {
+    id: "classic-associations",
+    label: "Associations (classic)",
+    node: <div>Tab 1 Content</div>,
+  },
+  {
+    id: "dynamic-associations",
+    label: "Associations (dynamic)",
+    node: <div>Tab 2 Content</div>,
+  },
+  {
+    id: "profile",
+    label: "Profile",
+    node: (
+      <SectionContainer>
+        <Section
+          title="Section 1"
+          subtitle="Some subtitle text"
+          avatar={<Avatar>S1</Avatar>}
+        >
+          <ChipContainer>
+            <Chip label="NR3A1" />
+            <Chip label="Era" />
+            <Chip label="ER-alpha" />
+            <Chip label="estrogen receptor alpha" />
+            <Chip label="oestrogen receptor alpha" />
+            <Chip label="Estrogen receptor" />
+            <Chip label="Estradiol receptor" />
+          </ChipContainer>
+        </Section>
+        <Section
+          title="Section 2"
+          subtitle="Some subtitle text"
+          avatar={<Avatar>S2</Avatar>}
+        >
+          Content
+        </Section>
+        <Section
+          title="Section 3"
+          subtitle="Some subtitle text"
+          avatar={<Avatar>S3</Avatar>}
+        >
+          Content
+        </Section>
+      </SectionContainer>
+    ),
+  },
+];
+
+const symbol = "PCSK9";
+const name = "proprotein convertase subtilisin/kexin type 9";
 const GenePage: React.FC = () => (
-  <SectionContainer>
-    <Section
-      title="Section 1"
-      subtitle="Some subtitle text"
-      avatar={<Avatar>S1</Avatar>}
-    >
-      <ChipContainer>
-        <Chip label="NR3A1" />
-        <Chip label="Era" />
-        <Chip label="ER-alpha" />
-        <Chip label="estrogen receptor alpha" />
-        <Chip label="oestrogen receptor alpha" />
-        <Chip label="Estrogen receptor" />
-        <Chip label="Estradiol receptor" />
-      </ChipContainer>
-    </Section>
-    <Section
-      title="Section 2"
-      subtitle="Some subtitle text"
-      avatar={<Avatar>S2</Avatar>}
-    >
-      Content
-    </Section>
-    <Section
-      title="Section 3"
-      subtitle="Some subtitle text"
-      avatar={<Avatar>S3</Avatar>}
-    >
-      Content
-    </Section>
-  </SectionContainer>
+  <React.Fragment>
+    <PageTitle
+      title={symbol}
+      subtitle={name}
+      avatar={
+        <Avatar>
+          <GeneIcon />
+        </Avatar>
+      }
+    />
+    <Tabs defaultTabId="classic-associations" tabs={tabs} />
+  </React.Fragment>
 );
 
 export default GenePage;
