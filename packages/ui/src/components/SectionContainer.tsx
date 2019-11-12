bebe3a1dedc21a569678b45af8ecaaf68afe64a6
peatroot <garethpeat@gmail.com>
2019-11-12 16:07:50 +0000

    Support single section in SectionsContainer

--- packages/ui/src/components/SectionContainer.tsx
+++ packages/ui/src/components/SectionContainer.tsx
@@ -2,20 +2,35 @@ import * as React from "react";
 import { Box, Grid } from "@material-ui/core";
 import { SectionProps } from "./Section";
 
+type SectionType = React.ReactElement<SectionProps>;
+type SectionsType = React.ReactElement<SectionProps>[];
+
 type Props = {
   // TODO: try to find a way to reject children
   //       by type and not just by props
-  children: React.ReactElement<SectionProps>[];
+  children: SectionType | SectionsType;
 };
+
+function isSectionsType(
+  children: SectionType | SectionsType
+): children is SectionsType {
+  return children instanceof Array;
+}
 
 const SectionContainer: React.FC<Props> = ({ children }) => (
   <Box m={2}>
     <Grid container spacing={2}>
-      {children.map((child, i) => (
-        <Grid key={i} item xs={12}>
-          {child}
+      {isSectionsType(children) ? (
+        children.map((child, i) => (
+          <Grid key={i} item xs={12}>
+            {child}
+          </Grid>
+        ))
+      ) : (
+        <Grid item xs={12}>
+          {children}
         </Grid>
-      ))}
+      )}
     </Grid>
   </Box>
 );
